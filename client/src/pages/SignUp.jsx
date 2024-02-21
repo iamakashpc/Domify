2f7ee94e67ff36b91bb73844ed5e070441fe7727
AKASH P C <iamakashpc@gmail.com>
2024-02-21 19:06:37 +0530

    Add styling to the sign up page and add a link to the sign in page.

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,41 @@
-
-
+import {Link} from 'react-router-dom'
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
+	return (
+		<div className="p-3 mx-auto max-w-sm border mt-20">
+			<h1 className="text-3xl text-center font-semibold my-7 text-slate-800">
+				Sign Up{" "}
+			</h1>
+			<form className="flex flex-col gap-4">
+				<input
+					type="text"
+					className="border p-3 rounded-lg text-slate-800"
+					placeholder="Username"
+					id="username"
+				/>
+				<input
+					type="email"
+					className="border p-3 rounded-lg text-slate-800"
+					placeholder="Email"
+					id="email"
+				/>
+				<input
+					type="password"
+					className="border p-3 rounded-lg text-slate-800"
+					placeholder="Password"
+					id="username"
+				/>
+				<button className="bg-slate-800 text-white p-3 rounded-xl uppercase hover:opacity-85 disabled:opacity-80">
+					Sign up
+				</button>
+			</form>
+			<div className="flex gap-2 mt-5">
+				<p>Have an account?</p>
+				<Link to={"/sign-in"}>
+					<span className="text-blue-700">Sign in</span>
+				</Link>
+			</div>
+		</div>
+	);
+};
 
-export default SignUp
+export default SignUp;
