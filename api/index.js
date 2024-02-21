eca37b24320c8e6aab846777a3b70739e48773cf
AKASH P C <iamakashpc@gmail.com>
2024-02-21 16:07:14 +0530

    data base connected

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+	.connect(process.env.MONGO_DB_URI)
+	.then(() => {
+		console.log("MongoDb connected ");
+	})
+	.catch((err) => {
+		console.log(err);
+	});
 
 const app = express();
 
-app.listen(3000,(req,res)=>{
-    console.log('Server is running on port 3000');
-})
+app.listen(3000, (req, res) => {
+	console.log("Server is running on port 3000");
+});
