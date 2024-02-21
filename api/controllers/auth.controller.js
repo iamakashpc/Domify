98c1c79a3da8e84b899072af5d1abb7186e44a7c
AKASH P C <iamakashpc@gmail.com>
2024-02-21 23:56:07 +0530

    add JWT for user authentication.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
-
+import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
 	try {
 		const { username, email, password } = req.body;
@@ -18,7 +18,25 @@ export const signup = async (req, res, next) => {
 		res.status(201).json({ message: "User created successfully!" });
 	} catch (error) {
 		// Handle errors
-	
-		next(error)
+
+		next(error);
+	}
+};
+
+export const signin = async (req, res, next) => {
+	const { email, password } = req.body;
+	try {
+		const validUser = await User.findOne({ email });
+		if (!validUser) return next(404, "User Not Found");
+		const validPassword = bcryptjs.compareSync(password, validUser.password);
+		if (!validPassword) return next("Invalid Password!");
+		const token = jwt.sign({ id: validUser._id },process.env.JWT_SECRET);
+		const {password:pass,...rest}=validUser._doc
+		res
+			.cookie("access_token", token, { httpOnly: true })
+			.status(200)
+			.json(rest);
+	} catch (error) {
+		next(error);
 	}
 };
