1320893677dcb5f88bbe9bc39c7082f5dd6a477b
AKASH P C <iamakashpc@gmail.com>
2024-02-22 16:03:20 +0530

    Update Profile page with user information and update button

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,47 @@
-
-
+import { useSelector } from "react-redux";
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+	const { currentUser } = useSelector((state) => state.user);
+	return (
+		<div className="p-3 max-w-sm mx-auto">
+			<h1 className="text-3xl font-semibold text-center my-5">Profile Info</h1>
+			<form className="flex flex-col gap-4">
+				<img
+					src={currentUser.avatar}
+					alt="profile"
+					className="rounded-full h-28 w-28 object-cover cursor-pointer self-center mt-1"
+				/>
+				<input
+					type="text"
+					placeholder="username"
+					id="username"
+					className="border p-3 rounded-lg"
+				/>
+				<input
+					type="email"
+					placeholder="email"
+					id="email"
+					className="border p-3 rounded-lg"
+				/>
+				<input
+					type="text"
+					placeholder="password"
+					id="password"
+					className="border p-3 rounded-lg"
+				/>
+				<button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
+					update
+				</button>
+			</form>
+			<div className="flex justify-between mt-5">
+				<span className="text-red-700 cursor-pointer font-semibold">
+					Delete Account
+				</span>
+				<span className="text-blue-900 cursor-pointer font-semibold">
+					Sign Out
+				</span>
+			</div>
+		</div>
+	);
+};
 
-export default Profile
+export default Profile;
