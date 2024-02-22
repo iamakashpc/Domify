e02b11f0fe094e2c6eaa49a8ecabb82d58917f81
AKASH P C <iamakashpc@gmail.com>
2024-02-22 15:02:28 +0530

    Added private route for profile page and updated header component to display user avatar if logged in, otherwise display "Sign in" text.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,20 +6,22 @@ import Profile from "./pages/Profile"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import Header from "./components/Header"
+import PrivateRoute from "./components/PrivateRoute"
 const App = () => {
   return (
-    <BrowserRouter>
-    <Header/>
-    <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-        <Route path="/sign-in" element={<SignIn/>}/>
-        <Route path="/sign-up" element={<SignUp/>}/>
-    </Routes>
-
-    </BrowserRouter>
-  )
+		<BrowserRouter>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route element={<PrivateRoute />}>
+					<Route path="/profile" element={<Profile />} />
+				</Route>
+				<Route path="/sign-in" element={<SignIn />} />
+				<Route path="/sign-up" element={<SignUp />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App
