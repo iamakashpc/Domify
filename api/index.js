170084be81118f141b9a7359e411887bf7046000
AKASH P C <iamakashpc@gmail.com>
2024-02-23 16:14:28 +0530

    Update user controller and routes to add update user functionality and add cookie parser.

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import authRouter from './routes/auth.route.js'
 dotenv.config();
@@ -15,6 +16,7 @@ mongoose
 	});
 
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 
 
